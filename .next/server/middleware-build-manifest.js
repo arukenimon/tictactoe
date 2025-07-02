02e1c243c31d9413d70d79d32f5f3df3cd066e7f
Michael John Revilla <revilla.261443@gensan.sti.edu.ph>
2025-07-02 18:32:01 +0800

    x

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-ae131a237aa0d9e1.js","static/chunks/4bd1b696-e494a2b8ef128c6f.js","static/chunks/684-8fe3dc74895820be.js","static/chunks/main-app-2b2f7326f267b89b.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-ae131a237aa0d9e1.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-68a4efb29ad45f01.js","static/chunks/pages/_app-92f2aae776f86b9c.js"],"/_error":["static/chunks/webpack-ae131a237aa0d9e1.js","static/chunks/framework-f593a28cde54158e.js","static/chunks/main-68a4efb29ad45f01.js","static/chunks/pages/_error-71d2b6a7b832d02a.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
